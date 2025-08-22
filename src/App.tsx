b829cf592a169b3d44edaf62bb112393fdc00f2a
amall-j <amonovamal562@gmail.com>
2025-08-22 04:12:02 -0700

    mening o'zgarishlarim

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import Login from "./pages/login/Login";
 
 function App() {
-  return <div className="flex h-screen bg-slate-900 text-white container">
-    ff
-  </div>;
+  return (
+    <div className="flex h-screen bg-slate-900 text-white container">
+      <Login />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/assets/index.ts
@@ -0,0 +1,4 @@
+import LoginBg from "./Background.svg";
+import FingerIcon from "./Border (1).svg";
+import FaceIcon from "./Border.svg";
+export { LoginBg, FingerIcon, FaceIcon };
