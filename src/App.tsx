bb3592e9c20e3d7071a33b173d93e13882448852
sadriddinoov <bilolsadriddinov941@gmail.com>
2025-08-25 19:59:21 +0500

    merged

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Header from "./components/Header";
 import Statistika from "./pages/Statistic/Statistic";
 import KPI from "./pages/Kpi/Kpi";
 import UniversityDetail from "./pages/Statistic/components/UniversityDetail";
-import UniversityDetailStat from "./pages/Statistic/components/UniversityDetailStat";
+import UniversityDetailStat from "./pages/Statistic/components/univerDetailing pages/UniversityDetailStat";
 
 function DashboardLayout({
   title,
@@ -22,7 +22,7 @@ function DashboardLayout({
       <Sidebar />
       <div className="flex-1 flex flex-col">
         <Header title={title} extra={extra} />
-        <main className="flex-1 py-6 px-6">{children}</main>
+        <main className="flex-1 py-6 px-6 container">{children}</main>
       </div>
     </div>
   );
