7821be1b31d9ae27580408deebfe6129ec87041a
amall-j <amonovamal562@gmail.com>
2025-08-26 18:09:27 +0500

    Univer services list

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function DashboardLayout({
       <Sidebar />
       <div className="flex-1 flex flex-col">
         <Header title={title} extra={extra} />
-        <main className="flex-1 py-6 px-6 container">{children}</main>
+        <main className="flex-1 py-6 px-6 ">{children}</main>
       </div>
     </div>
   );
--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -2,4 +2,25 @@ import LoginBg from "./Background.svg";
 import PoliceIcon from "./SVG.svg";
 import Go from "./Group.svg";
 import DetIcon1 from "./detIcon1.svg";
-export { LoginBg, DetIcon1, PoliceIcon, Go };
+import Sxema from "./sxema.svg";
+import offenbicon1 from "./offenbicon1.svg";
+import offenbicon2 from "./offenbicon2.svg";
+import offenbicon3 from "./offenbicon3.svg";
+import offenbicon4 from "./offenbicon4.svg";
+import offenbicon5 from "./offenbicon5.svg";
+import yoshlar from "./SVG (1).svg";
+import talaba from "./talab.svg";
+export {
+  LoginBg,
+  DetIcon1,
+  PoliceIcon,
+  yoshlar,
+  talaba,
+  Go,
+  Sxema,
+  offenbicon1,
+  offenbicon2,
+  offenbicon3,
+  offenbicon4,
+  offenbicon5,
+};
